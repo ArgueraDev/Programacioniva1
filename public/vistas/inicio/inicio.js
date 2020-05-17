25c37e60da4ae09f6b4c2db5ccc1317055d6ca8d
ArgueraDev <robertoarguera@gmail.com>
2020-05-17 13:15:49 -0600

    chat

--- public/vistas/inicio/inicio.js
+++ public/vistas/inicio/inicio.js
@@ -15,7 +15,6 @@ var appInicio = new Vue({
         verbancaria: function () {
             fetch(`private/Modulos/informacion/procesos.php?proceso=buscarbancaria&informacion=${this.valor}`).then(resp => resp.json()).then(resp => {
                 this.bancaria = resp;
-                console.log(resp);
             });
         },
         verDiccionario: function () {
@@ -34,39 +33,3 @@ var appInicio = new Vue({
 $("#contribuir").click(function (e) {
     $(`#vistas`).load(`public/vistas/informacion/informacion.html`);
 });
-
-(function (d, t, e, m) {
-
-    // Async Rating-Widget initialization.
-    window.RW_Async_Init = function () {
-
-        RW.init({
-            huid: "458081",
-            uid: "7cfd98716eccbd16e57b78b2f15c7cb4",
-            source: "website",
-            options: {
-                "size": "medium",
-                "lng": "es",
-                "style": "crystal",
-                "isDummy": false
-            }
-        });
-        RW.render();
-    };
-    // Append Rating-Widget JavaScript library.
-    var rw, s = d.getElementsByTagName(e)[0],
-        id = "rw-js",
-        l = d.location,
-        ck = "Y" + t.getFullYear() +
-        "M" + t.getMonth() + "D" + t.getDate(),
-        p = l.protocol,
-        f = ((l.search.indexOf("DBG=") > -1) ? "" : ".min"),
-        a = ("https:" == p ? "secure." + m + "js/" : "js." + m);
-    if (d.getElementById(id)) return;
-    rw = d.createElement(e);
-    rw.id = id;
-    rw.async = true;
-    rw.type = "text/javascript";
-    rw.src = p + "//" + a + "external" + f + ".js?ck=" + ck;
-    s.parentNode.insertBefore(rw, s);
-}(document, new Date(), "script", "rating-widget.com/"));
